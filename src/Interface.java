f5a3a9fadceb5b1b52f3eaa2fc309f4bd9a68650
nanettip <nanettip@grinnell.edu>
2014-02-12 19:46:42 -0600

    finalinterface

--- src/Interface.java
+++ src/Interface.java
@@ -10,22 +10,23 @@ import java.io.PrintWriter;
 
 public class Interface
 {
+ 
   /**
-   * 
+   *
    * User interface that runs Calculator.java
-   * 
+   *
    * First expression entered follows "number operator number"
-   * 
+   *
    * Expressions that follow the first expression must be "operator number"
-   * 
+   *
    * Enter "clear" to reenter the first expression and clear memory
-   * 
+   *
    * Valid operators are + - * /
-   * 
+   *
    * Memory slots r0 - r7 To save an equation to memory, enter rX = expression
-   * 
+   *
    * Memory must be entered on first expression
-   * 
+   *
    */
   public static void main(String[] args)
     throws Exception
@@ -35,29 +36,28 @@ public class Interface
         new java.io.InputStreamReader(System.in);
     java.io.BufferedReader eyes = new java.io.BufferedReader(istream);
 
-    
+   
     // Initializing variables
     int counter = 0;
     String input = "";
-    String firstelement = "";
+    String firstElement = "";
     Fraction answer = Fraction.ZERO;
-    
+   
     // Read-Eval-Print Loop
-    while (input.compareTo("quit") != 0)
+    while (counter != 2)
       {
-        try
-        {
-        input = eyes.readLine();    
-        if (input.compareTo("quit") == 0)
+      
+        input = eyes.readLine();
+       
+        if (input.compareToIgnoreCase("quit") == 0)
           {
+            counter = 2;
             for (int i = 0; i < 8; i++)
               Calculator.evaluate("r" + i + " = 0");
           } // if user inputs quit
-        else if (input.compareTo("clear") == 0)
+        else if (input.compareToIgnoreCase("clear") == 0)
           {
             counter = 0;
-            for (int i = 0; i < 8; i++)
-              Calculator.evaluate("r" + i + " = 0");
           } // else if user inputs clear
         else if (counter == 0)
           {
@@ -70,22 +70,21 @@ public class Interface
             else
               {
                 answer = Calculator.evaluate(input);
-                pen.println(answer);
-                firstelement = answer.toString();
+                pen.print(answer + " ");
+                pen.flush();
+                firstElement = answer.toString();
                 counter++;
               } // else user does not want to save expression
           } // else if user enters first expression
         else
           {
-            answer = Calculator.evaluate(firstelement + " " + input);
-            pen.println(answer);
-            firstelement = answer.toString();
+            answer = Calculator.evaluate(firstElement + " " + input);
+            pen.print(answer + " ");
+            pen.flush();
+            firstElement = answer.toString();
           } // else user enters expressions after first expression
-        }
-        catch (Exception e)
-        {
-          throw new Exception ("Must be a valid input");
-        }
+     
       } // while(input != end)
-  } // main 
+   
+  } // main
 } // class Interface
